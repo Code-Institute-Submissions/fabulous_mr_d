e06e5fcd8a9a553972f148db947a2748c2eb3f67
kajamiko <kajaths@gmail.com>
2018-08-01 15:19:48 +0100

    Added some content and final touches

--- js/script_extra.js
+++ js/script_extra.js
@@ -3,7 +3,11 @@ var fun_facts=["Mr D is actually a half-breed Maine Coon.",
                 "Cats can't taste sweetness...Do not ever treat your cat with sweets!",
                 "Mr D's birthday is 1st November.",
                 "Cats use their whiskers to detect if they can fit through a space.",
-                "Mr D's favourite food brand is Sheba."
+                "Mr D's favourite food brand is Sheba.",
+                "A cat can’t climb head first down a tree because every claw on a cat’s paw points the same way. To get down from a tree, a cat must back down.",
+                "There are more than 500 million domestic cats in the world, with approximately 40 recognized breeds.",
+                "The group of words associated with cat (catt, cath, chat, katze) stem from the Latin catus, meaning domestic cat, as opposed to feles, or wild cat.",
+                "A cat can travel at a top speed of approximately 31 mph (49 km) over a short distance."
                 ];
 
 function get_fact(){
